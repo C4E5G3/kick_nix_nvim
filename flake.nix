a6e704251b8f11bf21de00977d76b78fff68b603
Glass <marcuswest@protonmail.com>
2024-08-28 15:46:22 +0200

    lightline

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
     flake-utils.url = "github:numtide/flake-utils";
     gen-luarc.url = "github:mrcjkb/nix-gen-luarc-json";
 
+  
     # Add bleeding-edge plugins here.
     # They can be updated with `nix flake update` (make sure to commit the generated flake.lock)
     # wf-nvim = {
